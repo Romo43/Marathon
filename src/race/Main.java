2642b102a44f6ed0d2b943e5d3912f24afc4b3cc
deadman327 <champion.pokemon327@gmail.com>
2021-09-20 22:41:38 -0500

    Niveles ahora si

--- src/race/Main.java
+++ src/race/Main.java
@@ -9,27 +9,29 @@ public class Main {
 		System.out.println( "The race starts" );
 		
 		
-		runners h_1 = new runners( "Usain Bolt" );
+		runners h_1 = new runners( "Usain Bolt", "Experto");
 		Thread h1 = new Thread( h_1 );
 		
-		runners h_2 = new runners( "Flash" );
+		runners h_2 = new runners( "Flash" ,"Novato");
 		Thread h2 = new Thread( h_2 );
 		
-		runners h_3 = new runners( "Speed" );
+		runners h_3 = new runners( "Speed" , "Avanzado");
 		Thread h3 = new Thread( h_3 );
-		
-		runners h_4 = new runners( "Sumperman" );
+
+		runners h_4 = new runners( "Sumperman" , "Experto");
 		Thread h4 = new Thread( h_4 );
-		
-		runners h_5 = new runners( "Shazam" );
+
+		runners h_5 = new runners( "Shazam" , "Avanzado");
 		Thread h5 = new Thread( h_5 );
-		
+
+
 		h1.start();
 		h2.start();
 		h3.start();
 		h4.start();
 		h5.start();
-		
+
+
 	}
-	
+
 }
--- src/race/runners.java
+++ src/race/runners.java
@@ -3,11 +3,13 @@ package race;
 public class runners implements Runnable {
 
 	String name;
+	String lvl;
+	int vel;
 	
-	public runners( String n ) {
+	public runners( String n, String h) {
 		
 		this.name = n;
-		
+		this.lvl = h;
 	}
 	
 	public String getName() {
@@ -15,13 +17,30 @@ public class runners implements Runnable {
 		return this.name;
 		
 	}
-	
+
+	public String getLevel(){
+
+		if(this.lvl=="Experto")
+		{
+			vel = (int) Math.floor(Math.random()*(1-18)+18);
+		}else if(this.lvl=="Avanzado"){
+			vel = (int) Math.floor(Math.random()*(25-15)+25);
+		}else{
+			vel = (int) Math.floor(Math.random()*(30-40)+40);
+		}
+
+		return this.lvl;
+	}
+
 	public void run() {
 		
 		int i = 1;
-		
+
+
+
+
 		System.out.println( "Runner " + getName() + " starts" );
-		
+
 		try {
 			
 			Thread.sleep(2000);
@@ -31,11 +50,20 @@ public class runners implements Runnable {
 			e.printStackTrace();
 			
 		}
-		
+
+
+
 		while( i < 11 ) {
 			
 			if(i < 10){
-				
+
+			getLevel();
+
+				try {
+					Thread.sleep(vel);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				System.out.println( getName() + ":  " + i + " km" );
 				
 			}else {
