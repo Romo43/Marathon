f095a207748efa55cd4bae180e47c2434ca1fdd9
deadman327 <champion.pokemon327@gmail.com>
2021-09-22 21:59:28 -0500

    Evento de tomar agua

--- src/race/runners.java
+++ src/race/runners.java
@@ -6,6 +6,12 @@ public class runners implements Runnable {
 	String lvl;
 	String nbr;
 	int vel;
+
+	public static int randomNum(int minimo,int maximo){
+
+		int num=(int)Math.floor(Math.random()*(maximo-minimo+1)+(minimo));
+		return num;
+	}
 	
 	public runners( String n, String h, String u ) {
 		
@@ -31,20 +37,32 @@ public class runners implements Runnable {
 
 		if( this.lvl == "Experto" ) {
 
-			this.vel = ( int ) Math.floor( Math.random() * ( 1 - 18 ) + 18 );
+			this.vel = randomNum(1,18);
 
 		}else if ( this.lvl == "Avanzado" ) {
 
-			this.vel = ( int ) Math.floor( Math.random() * ( 25 - 15 ) + 25 );
+			this.vel = randomNum(15,25);
 
 		} else {
 
-			this.vel = ( int ) Math.floor( Math.random() * ( 30 - 40 ) + 40 );
+			this.vel = randomNum(30,40);
 		}
 
 		return this.lvl;
 
 	}
+
+	public void event(int km)  {
+
+		if(randomNum(1,100)<=20){
+			try {
+					System.out.println( getName() + " " + getNumber() + " Ha decidido tomar agua en el:  " + km + " km" );
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	};
 
 	public void run() {
 		
@@ -53,16 +71,17 @@ public class runners implements Runnable {
 		System.out.println( "Runner " + getName() + " " + getNumber() + ":" + " starts" );
 
 		try {
-			
+
 			Thread.sleep(2000 );
-			
+
 		} catch( InterruptedException e ) {
-			
+
 			e.printStackTrace();
-			
+
 		}
 
 		getLevel();
+
 
 		while( i < 11 ) {
 
@@ -70,7 +89,7 @@ public class runners implements Runnable {
 
 				try {
 
-					Thread.sleep( vel );
+					Thread.sleep( vel * 400 );
 
 				} catch (InterruptedException e) {
 
@@ -79,6 +98,19 @@ public class runners implements Runnable {
 				}
 
 				System.out.println( getName() + " " + getNumber() + ":  " + i + " km" );
+
+				switch (i){
+					case 3:
+						event(i);
+						break;
+					case 6:
+						event(i);
+						break;
+					case 9:
+						event(i);
+						break;
+				}
+
 				
 			} else {
 				
